83ddee8c431bb40256ece292c3d4e65827c396c5
pmsu2007 <pmsu2007@naver.com>
2025-01-14 11:43:19 +0900

    feat : 블루-그린 배포 사전 작업 - production 환경 변수 - pm2 설정을 위한 준비

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
-require('dotenv').config();
+const dotenv = require('dotenv');
+dotenv.config();
+
+if (process.env.NODE_ENV === 'production') {
+    require('dotenv').config({ path: `./config/.env`});
+}
+
 const app = require('./app');
 
 const PORT = process.env.PORT || 8080;
+let isKeepAlive = false;
 
 app.listen(PORT, () => {
-    console.log(`API Server is running on http://localhost:${PORT}`);
+    isKeepAlive = true;
+    if (process.send) {
+        process.send('ready');
+    }
+    console.log(`API Server is running on port ${PORT}`);
+})
+
+process.on('SIGINT', () => {
+    app.close(() => {
+        console.log('server closed');
+        if (process.exit) {
+            process.exit(0);
+        }
+    })
 })
