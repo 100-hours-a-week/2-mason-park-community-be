6fdfa804612fffb5f5bfa0745278370860029703
pmsu2007 <pmsu2007@naver.com>
2025-01-18 16:58:21 +0900

    refactor : 게시글 테이블 분리 반영

--- models/postModel.js
+++ models/postModel.js
@@ -1,20 +1,30 @@
 const {time} = require("../utils/response");
 const response = require("../utils/response");
+
+/* 게시글 생성 */
 exports.save = async (conn, title, content, post_image, userId) => {
+    // INSERT POSTS
     const query = `INSERT INTO POSTS (title, content, post_image, user_id) VALUES (?, ?, ?, ?)`;
+    const result = await conn.query(query, [title, content, post_image, userId]);
+
+    // INSERT POST_METAS
+    const metaQuery = `INSERT INTO POST_METAS (post_id) VALUES (?)`;
+    await conn.query(metaQuery, [Number(result.insertId)]);
 
     // insertId : bigint | Number(insertId)
-    return await conn.query(query, [title, content, post_image, userId]);
+    return result;
 }
 
 /* 게시글 목록 조회 */
+// TODO : no offset pagination
 exports.findAll = async (conn, limit, offset, viewerId) => {
+    // SELECT POSTS (with USERS, POST_METAS)
     const contentQuery = `SELECT 
                            p.post_id,
                            p.title, 
-                           p.comment_count,
-                           p.view_count,
-                           p.thumb_count,
+                           pm.comment_count,
+                           pm.view_count,
+                           pm.thumb_count,
                            p.created_at,
                            u.user_id,
                            u.nickname,
@@ -23,6 +33,7 @@ exports.findAll = async (conn, limit, offset, viewerId) => {
                                WHERE pt.post_id = p.post_id AND pt.user_id = ?) AS is_thumbs
                           FROM POSTS AS p 
                           JOIN USERS AS u ON p.user_id = u.user_id
+                          JOIN POST_METAS AS pm ON p.post_id = pm.post_id
                           ORDER BY p.created_at DESC
                           LIMIT ? OFFSET ?
     `;
@@ -60,9 +71,9 @@ exports.findByIdWithUser = async (conn, viewerId, postId) => {
                     p.post_id,
                     p.title, 
                     p.content,
-                    p.comment_count,
-                    p.view_count,
-                    p.thumb_count,
+                    pm.comment_count,
+                    pm.view_count,
+                    pm.thumb_count,
                     p.post_image,
                     p.created_at,
                     u.user_id,
@@ -72,6 +83,7 @@ exports.findByIdWithUser = async (conn, viewerId, postId) => {
                                WHERE pt.post_id = p.post_id AND pt.user_id = ?) AS is_thumbs 
                    FROM POSTS AS p
                    JOIN USERS AS u ON p.user_id = u.user_id
+                   JOIN POST_METAS AS pm ON p.post_id = pm.post_id
                    WHERE p.post_id = ?
     `;
 
@@ -101,9 +113,9 @@ exports.findById = async (conn, postId) => {
                     p.post_id,
                     p.title, 
                     p.content,
-                    p.comment_count,
-                    p.view_count,
-                    p.thumb_count,
+                    pm.comment_count,
+                    pm.view_count,
+                    pm.thumb_count,
                     p.post_image,
                     p.created_at,
                     u.user_id,
@@ -111,6 +123,7 @@ exports.findById = async (conn, postId) => {
                     u.profile_image
                    FROM POSTS AS p
                    JOIN USERS AS u ON p.user_id = u.user_id
+                   JOIN POST_METAS AS pm ON p.post_id = pm.post_id
                    WHERE p.post_id = ?
     `;
 
@@ -119,6 +132,7 @@ exports.findById = async (conn, postId) => {
     return row; // Post Detail Object | undefined
 }
 
+/* 게시글 수정 */
 exports.update = async (conn, title, content, post_image, postId) => {
     const query = `UPDATE POSTS SET
                     title = ?, 
@@ -130,9 +144,9 @@ exports.update = async (conn, title, content, post_image, postId) => {
     return await conn.query(query, [title, content, post_image, postId]);
 }
 
-/* 조회수 증가 */
+/* 조회 수 증가 */
 exports.incrementViewCount = async (conn, postId) => {
-    const query = `UPDATE POSTS SET
+    const query = `UPDATE POST_METAS SET
                     view_count = view_count + 1
                    WHERE post_id = ?
     `
@@ -140,9 +154,9 @@ exports.incrementViewCount = async (conn, postId) => {
     return await conn.query(query, [postId]);
 }
 
-/* 댓글수 증가 */
+/* 댓글 수 증가 */
 exports.incrementCommentCount = async (conn, postId) => {
-    const query = `UPDATE POSTS SET
+    const query = `UPDATE POST_METAS SET
                     comment_count = comment_count + 1
                    WHERE post_id = ?
     `
@@ -150,9 +164,9 @@ exports.incrementCommentCount = async (conn, postId) => {
     return await conn.query(query, [postId]);
 }
 
-/* 댓글수 감소 */
+/* 댓글 수 감소 */
 exports.decrementCommentCount = async (conn, postId) => {
-    const query = `UPDATE POSTS SET
+    const query = `UPDATE POST_METAS SET
                     comment_count = comment_count - 1
                    WHERE post_id = ?
     `
@@ -160,9 +174,9 @@ exports.decrementCommentCount = async (conn, postId) => {
     return await conn.query(query, [postId]);
 }
 
-/* 좋아요수 증가 */
+/* 좋아요 수 증가 */
 exports.incrementThumbsCount = async (conn, postId) => {
-    const query = `UPDATE POSTS SET
+    const query = `UPDATE POST_METAS SET
                     thumb_count = thumb_count + 1
                    WHERE post_id = ?
     `
@@ -170,9 +184,9 @@ exports.incrementThumbsCount = async (conn, postId) => {
     return await conn.query(query, [postId]);
 }
 
-/* 좋아요수 감소 */
+/* 좋아요 수 감소 */
 exports.decrementThumbsCount = async (conn, postId) => {
-    const query = `UPDATE POSTS SET
+    const query = `UPDATE POST_METAS SET
                     thumb_count = thumb_count - 1
                    WHERE post_id = ?
     `
@@ -180,6 +194,7 @@ exports.decrementThumbsCount = async (conn, postId) => {
     return await conn.query(query, [postId]);
 }
 
+/* 게시글 삭제 */
 exports.deleteById = async (conn, postId) => {
     const query = `DELETE FROM POSTS WHERE post_id = ?`;
 
