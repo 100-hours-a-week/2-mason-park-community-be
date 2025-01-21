dcd5673f3ad57577d856a4557e2f1c291eb14123
pmsu2007 <pmsu2007@naver.com>
2025-01-21 13:25:19 +0900

    feat : 서버 실행 시 어드민 계정 생성

--- db/maria.js
+++ db/maria.js
@@ -8,13 +8,5 @@ const pool = maria.createPool({
     database: process.env.DATABASE_NAME,
 })
 
-pool.getConnection()
-    .then(() => {
-        console.log('커넥션 풀 상태 양호');
-    })
-    .catch(error => {
-        console.error('커넥션 풀 상태 오류:', error);
-    });
-
 module.exports = pool;
 
--- server.js
+++ server.js
@@ -10,12 +10,24 @@ const app = require('./app');
 const PORT = process.env.PORT || 8080;
 let isKeepAlive = false;
 
+const pool = require('./db/maria');
+const crypto = require("crypto-js");
+
 app.listen(PORT, () => {
     isKeepAlive = true;
     if (process.send) {
         process.send('ready');
     }
     console.log(`API Server is running on port ${PORT}`);
+
+    pool.getConnection()
+        .then(async (conn) => {
+            console.log('커넥션 풀 상태 양호');
+            await createAdmin(conn);
+        })
+        .catch(error => {
+            console.error('커넥션 풀 상태 오류:', error);
+        });
 })
 
 process.on('SIGINT', () => {
@@ -26,3 +38,33 @@ process.on('SIGINT', () => {
         }
     })
 })
+
+const createAdmin = async (conn) => {
+    console.log('어드민 계정 생성 중...');
+
+    const email = process.env.ADMIN_EMAIL;
+    const password = process.env.ADMIN_PASSWORD;
+    const role = 'ADMIN';
+    const nickname = '방장';
+    if (!email || !password) {
+        console.log('어드민 계정 정보가 환경변수에 설정되지 않았습니다');
+        return;
+    }
+
+    // 어드민 계정이 이미 있는지 확인
+    const rows = await conn.query(`SELECT 1 FROM USERS WHERE email = ? LIMIT 1`, [email]);
+    if (rows.length > 0) {
+        console.log('어드민 계정이 이미 존재합니다.');
+        return;
+    }
+
+    const encryptedPassword = crypto.AES.encrypt(password, process.env.CRYPTO_SECRET_KEY).toString();
+
+    // 어드민 계정 생성
+    await conn.query(
+        `INSERT INTO USERS (email, password, nickname, role) VALUES (?, ?, ?, ?)`,
+        [email, encryptedPassword, nickname, role]
+    );
+
+    console.log('어드민 계정 생성 완료...');
+}
